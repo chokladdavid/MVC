f4790bfd386b872093de2fc6518f83b8dd083fdb
chokladdavid <david.ramvall@comhem.se>
2016-05-17 13:48:14 +0200

    Adding a controller

---
+++ MVC_Lab_1/MVC_Lab_3_MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_Lab_3_MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            //return "This is the Welcome action method...";
+            //return HtmlEncoder.Default.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            return HtmlEncoder.Default.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
